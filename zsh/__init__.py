8ae5e78a84d6879a3242cdca204483a14355bcd5
Andrew Bass <andybass009@gmail.com>
2015-09-25 11:07:43 -0400

    Add ZSH module

---
+++ zsh/__init__.py
@@ -0,0 +1,14 @@
+
+import dotfiles
+import sys
+
+class Zsh(dotfiles.Module):
+    overriden_dests = {
+        "zshrc": "~/.zshrc",
+        "glen-magic.zsh-theme": "~/.oh-my-zsh/custom/themes/glen-magic.zsh-theme"
+    }
+
+    def install(self):
+        dotfiles.process('sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"')
+
+        yield "ZSH installed"
